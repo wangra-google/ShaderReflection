4f54d8f233c19abd237503727a4d7d7abef430cd
wangra-google <wangra@google.com>
2021-12-26 16:53:52 -0500

    use lib

--- src/ShaderReflection.cpp
+++ src/ShaderReflection.cpp
@@ -1,12 +1,28 @@
 ﻿// ShaderReflection.cpp : Defines the entry point for the application.
 //
 
+#include <wrl.h>
+#include "dxcapi.h"
 #include "ShaderReflection.h"
 
 using namespace std;
+using Microsoft::WRL::ComPtr;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	ComPtr<IDxcUtils> pUtils;
+	DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(pUtils.GetAddressOf()));
+
+	/*IDxcUtils* pUtils;
+	DxcCreateInstance(CLSID_DxcUtils, __uuidof(IDxcUtils), reinterpret_cast<void**>(&pUtils));*/
+
+	/*IDxcLibrary* dxcLib;
+	if (DxcCreateInstance(CLSID_DxcLibrary, __uuidof(IDxcLibrary),
+		reinterpret_cast<void**>(&dxcLib)) != S_OK)
+	{
+		return E_FAIL;
+	}*/
+
+	cout << "Hello ShaderReflection!" << endl;
 	return 0;
 }
